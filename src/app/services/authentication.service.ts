a65bbd12cc8d0c3527f0f9f10abb7778cae7d1de
Ubuntu <ubuntu@ip-172-31-41-20.ec2.internal>
2019-12-06 03:04:40 +0000

    Done

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -3,17 +3,9 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 @Injectable()
 export class AuthenticationService {
-<<<<<<< HEAD
   private authUrl: string;
   constructor(private httpclient: HttpClient) {
     this.authUrl = 'http://localhost:3000/auth/v1/';
-=======
-
-  private authUrl:string;
-  constructor(private httpclient:HttpClient)
-  {
-    this.authUrl="http://localhost:3000/auth/v1/";
->>>>>>> 86003c8628e4b921c85994b59f02f65eab493bd9
   }
   authenticateUser(data) {
     return this.httpclient.post(this.authUrl, data);
